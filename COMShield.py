d8ecbe333a381f6b14b2dd5a78c1c696c789f88e
Sulaiman Refaee <79332891+sulaimvn@users.noreply.github.com>
2023-08-12 16:46:29 +0300

    Update COMShield.py

--- COMShield.py
+++ COMShield.py
@@ -25,7 +25,9 @@ WARNING = Colors.YELLOW + "WARNING: " + Colors.ENDC
 ALERT = Colors.RED + "ALERT: " + Colors.ENDC
 INFO = Colors.GREY + "INFO: " + Colors.ENDC
 ERROR = Colors.RED + "ERROR: " + Colors.ENDC
-    
+
+
+
 csv_rows = []
 rawcopy_path = os.path.join(os.path.dirname(__file__), "RawCopy-master", "RawCopy.exe")
 source_usrclass_pattern = r"C:\Users\{}\AppData\Local\Microsoft\Windows\UsrClass.dat"
@@ -35,37 +37,30 @@ clear_dir = os.path.join(os.path.dirname(__file__), "output")
 recmd_path = os.path.join(os.path.dirname(__file__), "RECmd", "RECmd.exe")
 output_csv_file = os.path.join(os.path.dirname(__file__))
 
+
+
 devnull = open(os.devnull, 'w')
 def copy_user_profile_data():
-    
     rawcopy_path = os.path.join(os.path.dirname(__file__), "RawCopy-master", "RawCopy.exe")
-
-    source_usrclass_pattern = r"C:\Users\{}\AppData\Local\Microsoft\Windows\UsrClass.dat"
-    
+    source_usrclass_pattern = r"C:\Users\{}\AppData\Local\Microsoft\Windows\UsrClass.dat"    
     source_ntuser_pattern = r"C:\Users\{}\NTUSER.DAT"
-
     destination_directory = os.path.join(os.path.dirname(__file__), "output", "userprofile")
-
     devnull = open(os.devnull, 'w')
 
     for user in os.listdir(r"C:\Users"):
 
         source_usrclass_file = source_usrclass_pattern.format(user)
         source_ntuser_file = source_ntuser_pattern.format(user)
-
         usrclass_destination_file = os.path.join(destination_directory, user, "UsrClass.dat")
         ntuser_destination_file = os.path.join(destination_directory, user, "NTUSER.DAT")
-
         os.makedirs(os.path.dirname(usrclass_destination_file), exist_ok=True)
         os.makedirs(os.path.dirname(ntuser_destination_file), exist_ok=True)
-
         subprocess.run([rawcopy_path, "/FileNamePath:" + source_usrclass_file, "/OutputPath:" + os.path.dirname(usrclass_destination_file), "/OutputFileName:" + "UsrClass.dat"], stdout=devnull, stderr=subprocess.STDOUT)
         subprocess.run([rawcopy_path, "/FileNamePath:" + source_ntuser_file, "/OutputPath:" + os.path.dirname(ntuser_destination_file), "/OutputFileName:" + "NTUSER.DAT"], stdout=devnull, stderr=subprocess.STDOUT)
 
         try:
             for user_dir in os.listdir(destination_directory):
                 user_dir_path = os.path.join(destination_directory, user_dir)
-
                 usrclass_file_path = os.path.join(user_dir_path, "UsrClass.dat")
                 ntuser_file_path = os.path.join(user_dir_path, "NTUSER.DAT")
                 if not os.path.exists(usrclass_file_path) or not os.path.exists(ntuser_file_path):
@@ -73,56 +68,53 @@ def copy_user_profile_data():
                         file_path = os.path.join(user_dir_path, file)
                         os.remove(file_path)
                     os.rmdir(user_dir_path)
+                    
         except OSError as e:
             if e.errno == 39:
                 ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
             else:
                 raise e
-
 devnull.close()
+
+
 
 def get_sids():
     reg_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProfileList")
-
     num_subkeys = winreg.QueryInfoKey(reg_key)[0]
-
     sids = []
 
     for i in range(num_subkeys):
         sid = winreg.EnumKey(reg_key, i)
         if re.match("^S-1-5-21-\\d+-\\d+-\\d+-\\d+$", sid):
             sids.append(sid)
-
     winreg.CloseKey(reg_key)
 
     return sids
 
-def run_recmd(sid):
-    
-    for user in os.listdir(os.path.join(os.path.dirname(__file__), "output", "userprofile")):
-        
-        input_file_path = os.path.join(destination_directory, user, "UsrClass.dat")
 
-        output_file_path = os.path.join(destination_directory, user, sid, "UsrClass_CLSID.txt")
-        
+
+def run_recmd(sid):
+    output_directory = os.path.join(os.path.dirname(__file__), "output", "userprofile")
+
+    for user in os.listdir(output_directory):
+        user_directory = os.path.join(output_directory, user)
+        input_file_path = os.path.join(user_directory, "UsrClass.dat")
+        output_file_path = os.path.join(user_directory, sid, "UsrClass_CLSID.txt")
         os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
-        
-        command = r"{} -f {} --kn {}_Classes\CLSID\ --nl".format(recmd_path, input_file_path, sid)
-        
-        subprocess.run(command, shell=True, stdout=open(output_file_path, "w"))
+        command = '{} -f "{}" --kn "{}_Classes\CLSID" --nl'.format(recmd_path, input_file_path, sid)
+        subprocess.run(command, shell=True, stdout=open(output_file_path, "w"), text=True)
         
         with open(output_file_path, "r") as f:
             output = f.read()
 
         clsid_pattern = re.compile(r"\{?[0-9A-F]{8}-?[0-9A-F]{4}-?[0-9A-F]{4}-?[0-9A-F]{4}-?[0-9A-F]{12}\}?", re.IGNORECASE)
         clsid_values = clsid_pattern.findall(output)
-
         clsid(clsid_values, input_file_path, destination_directory, sid, user)
+
+
 		
 def clsid(clsid_values, input_file_path, destination_directory, sid, user):
-
     data_regex = re.compile(r"Data:\s+([^\s\(\)]+)")
-
     data_dict = {}
 
     for clsid in clsid_values:
@@ -130,10 +122,9 @@ def clsid(clsid_values, input_file_path, destination_directory, sid, user):
         output_clsid_dir = os.path.join(destination_directory, user, sid, clsid)
         os.makedirs(output_clsid_dir, exist_ok=True)
         output_clsid = os.path.join(output_clsid_dir, "CLSID.txt")
-
-        output = subprocess.run(iProcsvr32_command, shell=True, stdout=subprocess.PIPE).stdout.decode()
-        
+        output = subprocess.run(iProcsvr32_command, shell=True, stdout=subprocess.PIPE).stdout.decode()      
         match = data_regex.search(output)
+        
         if match:
             data = match.group(1)
             data_dict[clsid] = data
@@ -141,15 +132,13 @@ def clsid(clsid_values, input_file_path, destination_directory, sid, user):
     output_csv_file = os.path.join(os.path.dirname(__file__), "allusers_output.csv")
     
     with open(output_csv_file, "a", newline="") as f:
-        writer = csv.writer(f)
-        
-        
+        writer = csv.writer(f)      
         for clsid, data in data_dict.items():
             writer.writerow([user, clsid, data])
-            
+
+
+           
 def get_hklm():
-   
-    # Get the subkeys in HKEY_LOCAL_MACHINE\Software\Classes\CLSID
     key_path_local = r"Software\Classes\CLSID"
     key_local = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path_local, 0, winreg.KEY_READ | winreg.KEY_WOW64_64KEY)
     subkey_names_local = []
@@ -164,9 +153,8 @@ def get_hklm():
                 break
         i += 1
     winreg.CloseKey(key_local)
-    
-    # Store the values from HKEY_LOCAL_MACHINE\Software\Classes\CLSID in a dictionary
     values_dict_local = {}
+    
     for subkey_name in subkey_names_local:
         key_path_local = fr"Software\Classes\CLSID\{subkey_name}\InProcServer32"
         try:
@@ -194,38 +182,30 @@ def get_hklm():
             print(ERROR + time.ctime() + f"\tError accessing key {key_path_local}: {e}")
         finally:
             winreg.CloseKey(key_local)
-    
-    output_csv_file = os.path.join(os.path.dirname(__file__), "hklm_output.csv")
-    
+            
+    output_csv_file = os.path.join(os.path.dirname(__file__), "hklm_output.csv")   
     pattern = r"(\{[0-9A-F-]+\})_Default"
-
     with open(output_csv_file, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(['CLSID', 'Data'])
 
-
         for key, value in values_dict_local.items():
             matches = re.search(pattern, key)
-
             if matches:
                 clsid = matches.group(1)
-                
                 clsid_key = clsid + "_Default"
-                
                 extracted_value = values_dict_local.get(clsid_key)
-                
                 writer.writerow([clsid, extracted_value])
 
-def compare_registry_values(fname):
 
+
+def compare_registry_values(fname):
     all_users_file = os.path.join(os.path.dirname(__file__), "allusers_output.csv")
     hklm_file = os.path.join(os.path.dirname(__file__), "hklm_output.csv")
     final_csv = os.path.join(os.path.dirname(__file__), fname)
-    
     all_file_contents = {}
     hklm_contents = {}
    
-    # Read HKLM file and populate hklm_contents dictionary
     with open(hklm_file, "r", newline="") as f:
         reader = csv.reader(f)
         for row in reader:
@@ -234,7 +214,6 @@ def compare_registry_values(fname):
                 data = row[1]
                 hklm_contents[clsid] = data
 
-    # Read all users file and compare with hklm_contents
     differences = []
     with open(all_users_file, "r", newline="") as f:
         reader = csv.reader(f)
@@ -248,12 +227,13 @@ def compare_registry_values(fname):
                         differences.append([user, clsid, data, hklm_contents[clsid]])
         length = len(differences)
                     
-    # Write differences to output CSV file
     with open(final_csv, "w", newline="") as f:
         writer = csv.writer(f)
         for i in range(length):
             writer.writerow(differences[i])
-            
+
+
+
 def main():
     logo = '''
    _____ ____  __  __  _____ _     _      _     _ 
@@ -270,17 +250,23 @@ def main():
     init()
     parser = argparse.ArgumentParser(prog='COMShield', usage='\t\tpython.exe COMShield.py [options]', description='COMShield tool for Windows compares the registry values of a specific key between HKEY_USERS (HKU) and HKEY_LOCAL_MACHINE (HKLM) in the CLSID registry. It detects any differences between the two and alerts the user if any discrepancies are found. \n\n Emaples:\n\n'
           '\tpython.exe %(prog)s \n'
-          '\tpython.exe %(prog)s --output output.csv\n'
+          '\tpython.exe %(prog)s --path C:\\Users\\COM\\Desktop --output output.csv\n'
           '\tpython.exe %(prog)s --print', formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
     parser.add_argument("--output", help="Name of the CSV file to save the results.",  nargs='?')
+    parser.add_argument("--path", help="Path of the CSV file to save the results.")
     parser.add_argument("--print", help="Print the output in the console.", action="store_true")
     args = parser.parse_args()
     
     print(Colors.GREEN + "\t" + "COMShield by Sulaiman Refaee -" + " Version: 0.1" + Colors.ENDC)
     print(Colors.GREEN + "\t" + "Twitter: " + "@sulaimvn" + Colors.ENDC)
     print()
-    
+    try:
+        if clear_dir:
+            shutil.rmtree(clear_dir)
+    except OSError:
+        pass
+
     path = os.path.join(os.path.dirname(__file__))
 
     if " " in path:
@@ -288,74 +274,63 @@ def main():
         sys.exit(1)
         
     else:
-        if args.print:
+        copy_user_profile_data()
+        sids = get_sids()
+        for sid in sids:
+            run_recmd(sid)
+        shutil.rmtree(clear_dir)
+        get_hklm()
+        current_time = datetime.datetime.now()
+        formatted_time = current_time.strftime("%Y%m%d_%H%M%S")
+        default_name = formatted_time + "_COMShield.csv"
+        if args.output:
+            if args.path:
+                output_path = os.path.join(args.path, args.output)
+                compare_registry_values(output_path)
+                if args.print:
+                    command = 'type "{}"'.format(output_path)
+                    subprocess.run(command, shell=True)
+            else:
+                output_path = os.path.join(os.path.dirname(__file__), args.output)
+                compare_registry_values(output_path)
+                if args.print:
+                    command = 'type "{}"'.format(output_path)
+                    subprocess.run(command, shell=True)
 
-            copy_user_profile_data()
+            print("Results saved to: " + Colors.GREEN + output_path + Colors.ENDC)
+        
+        elif args.path:
+            output_path = os.path.join(args.path, default_name)
+            compare_registry_values(output_path)
+            print("Results saved to: " + Colors.GREEN + output_path + Colors.ENDC)
 
-            sids = get_sids()
-            
-            for sid in sids:
-                run_recmd(sid)
-            shutil.rmtree(clear_dir)
-            
-            get_hklm()
-            current_time = datetime.datetime.now()
-            formatted_time = current_time.strftime("%Y%m%d_%H%M%S")
-            default_name = formatted_time + "_COMShield.csv"
+        elif args.print and not args.output:
             compare_registry_values(default_name)
             print_file = os.path.join(os.path.dirname(__file__), default_name)
             command = 'type "{}"'.format(print_file)
             subprocess.run(command, shell=True)
             os.remove(print_file)
-              
-
-                
-        elif not args.output:
-        
-            copy_user_profile_data()
-
-            sids = get_sids()
-
-            for sid in sids:
-                run_recmd(sid)
-            shutil.rmtree(clear_dir)
-            
-            get_hklm()
-            current_time = datetime.datetime.now()
-            formatted_time = current_time.strftime("%Y%m%d_%H%M%S")
-            default_name = formatted_time + "_COMShield.csv"
+     
+  
+        elif not args.output and not args.path and not args.print:
             compare_registry_values(default_name)
             print("Results saved to: " + Colors.GREEN + os.path.join(os.path.dirname(__file__), default_name) + Colors.ENDC)
+  
 
-            
-        elif args.output:
-            copy_user_profile_data()
-
-            sids = get_sids()
-
-            for sid in sids:
-                run_recmd(sid)
-            shutil.rmtree(clear_dir)
-            
-            get_hklm()
-            user_name = args.output
-            
-            compare_registry_values(user_name)
-            print("Results saved to: " + Colors.GREEN + os.path.join(os.path.dirname(__file__), args.output) + Colors.ENDC)
-            
-
-            
-            
+                   
         hklmfile = os.path.join(os.path.dirname(__file__), "hklm_output.csv")
         allusersfile = os.path.join(os.path.dirname(__file__), "allusers_output.csv")
         os.remove(hklmfile)
         os.remove(allusersfile)
         
-
+        
+        
 try:
     main()
+    
 except KeyboardInterrupt:
     print(WARNING + time.ctime() + " [CTRL + C]? OK...")
     sys.exit(0)
+    
 except Exception as e:
     print(ERROR + time.ctime() + " Unknown error: " + str(e))
